f1b84fe48da6e806aa16a987be6e333e203ef7bb
Marcela <marcelasouza200515@gmail.com>
2024-10-08 10:29:49 -0300

    desafio

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,23 @@
+package Aprimoramento;
+
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe o número");
+		int num1 = entrada.nextInt();
+		
+		System.out.println("Informe o número");
+		int num2 = entrada.nextInt();
+		
+		int resultado = num1 + num2;
+		System.out.println("O resultado é " + resultado);		
+		
+		entrada.close();
+	}
+
+}
